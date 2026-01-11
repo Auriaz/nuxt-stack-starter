c4b9bc150f715eaa0a9555798714b6d05ea03f47
Auriaz <a.stankiewicz7@gmail.com>
2026-01-11 18:45:12 +0100

    feat: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'release',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 120],
+  },
+}
